e8da640c109848668e0a6707570163d731daf3c2
Niko4ka <niko4ka.1993@mail.ru>
2019-01-12 21:59:32 +0300

    Added force touch for movie cell in ResultsTVC

--- Film App/Search/ResultsTableViewController.swift
+++ Film App/Search/ResultsTableViewController.swift
@@ -24,6 +24,11 @@ class ResultsTableViewController: UITableViewController {
 
         self.tableView.tableFooterView = UIView()
         self.tableView.register(UINib(nibName: "CollectionTableViewCell", bundle: nil), forCellReuseIdentifier: "CollectionCell")
+        
+        if traitCollection.forceTouchCapability == .available {
+            print("Available")
+            registerForPreviewing(with: self, sourceView: view)
+        }
     }
 
     // MARK: - Table view data source
@@ -70,4 +75,34 @@ extension ResultsTableViewController: MovieCoordinator {
     
 }
 
-
+extension ResultsTableViewController: UIViewControllerPreviewingDelegate {
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        
+        guard let tableCellIndexPath = tableView.indexPathForRow(at: location),
+            let tableViewCell = tableView.cellForRow(at: tableCellIndexPath) as? CollectionTableViewCell,
+            let collectionView = tableViewCell.itemsCollectionView else { return nil }
+        
+        let collectionViewLocation = tableView.convert(location, to: collectionView)
+        
+        guard let collectionViewIndexPath = collectionView.indexPathForItem(at: collectionViewLocation),
+            let collectionViewCell = collectionView.cellForItem(at: collectionViewIndexPath) as? ItemCollectionViewCell else { return nil }
+        
+        let storyboard = UIStoryboard(name: "Movie", bundle: nil)
+        guard let movieController = storyboard.instantiateViewController(withIdentifier: "MovieTableViewController") as? MovieTableViewController else { return nil }
+        
+        movieController.movieId = collectionViewCell.objectID
+        movieController.mediaType = collectionViewCell.mediaType
+        movieController.navigator = navigator
+        movieController.preferredContentSize = CGSize(width: 0.0, height: 500.0)
+        previewingContext.sourceRect = collectionViewCell.frame
+        
+        return movieController
+    }
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+        show(viewControllerToCommit, sender: self)
+    }
+    
+    
+}
