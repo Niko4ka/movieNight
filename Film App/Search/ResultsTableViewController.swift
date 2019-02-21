c6397ec73d918b67f09eed50faa21904044087e6
Niko4ka <niko4ka.1993@mail.ru>
2019-02-22 00:23:14 +0800

    ResultsTableViewController + UIViewControllerPreviewingDelegate

--- Film App/Search/ResultsTableViewController.swift
+++ Film App/Search/ResultsTableViewController.swift
@@ -87,37 +87,6 @@ class ResultsTableViewController: UITableViewController, ColorThemeCellObserver 
 
 }
 
-extension ResultsTableViewController: UIViewControllerPreviewingDelegate {
-    
-    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
-        
-        guard let tableCellIndexPath = tableView.indexPathForRow(at: location),
-            let tableViewCell = tableView.cellForRow(at: tableCellIndexPath) as? CollectionTableViewCell,
-            let collectionView = tableViewCell.itemsCollectionView else { return nil }
-        
-        let collectionViewLocation = tableView.convert(location, to: collectionView)
-        
-        guard let collectionViewIndexPath = collectionView.indexPathForItem(at: collectionViewLocation),
-            let collectionViewCell = collectionView.cellForItem(at: collectionViewIndexPath) as? ItemCollectionViewCell else { return nil }
-        
-        let storyboard = UIStoryboard(name: "Movie", bundle: nil)
-        guard let movieController = storyboard.instantiateViewController(withIdentifier: "MovieTableViewController") as? MovieTableViewController else { return nil }
-        
-        movieController.movieId = collectionViewCell.objectID
-        movieController.mediaType = collectionViewCell.mediaType
-        movieController.navigator = navigator
-        movieController.preferredContentSize = CGSize(width: 0.0, height: 500.0)
-        previewingContext.sourceRect = collectionViewCell.frame
-        
-        return movieController
-    }
-    
-    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
-        show(viewControllerToCommit, sender: self)
-    }
-    
-}
-
 extension ResultsTableViewController {
     
     func darkThemeEnabled() {
